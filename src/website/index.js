dc8f44cb599c893fe20f53c495b448a15a7af894
Moyano, Pedro <pedro_moyano@mcafee.com>
2018-05-14 19:41:38 -0300

    Elimine archivos inecesarios

--- src/website/index.js
+++ src/website/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import bootstrap from 'bootstrap/scss/bootstrap.scss';
 import App from './containers/App'
 
-const Index = () => {
-  return <div>
-          <App/>
-        </div>;
-};
+const Index = () =>
+  (<Router>
+    <div>
+      <Route exact path="/" component={App} />
+      <Route path="/pepe" render={() => <h1>Pepe</h1>}  />
+    </div>
+  </Router>)
+
 
 ReactDOM.render(<Index />, document.getElementById("main-content"));
--- webpack.config.js
+++ webpack.config.js
@@ -120,13 +120,13 @@ module.exports = {
       }
     ]
   },
-//   devServer: {
-//     port: 3000,
-//     open: true,
-//     proxy: {
-//       '/api': 'http://localhost:8080'
-//     }
-//   },
+  devServer: {
+    port: 3000,
+    open: true,
+    proxy: {
+      '/api': 'http://localhost:8080'
+    }
+  },
   plugins: [
     new CleanWebpackPlugin([outputDirectory]),
     new HtmlWebpackPlugin({
