1ba039bd85641f3302764c4063954466579a6ffd
Pedro Emiliano Moyano Cires <pedro.emiliano@gmail.com>
2018-05-13 21:05:35 -0300

    Hice unos cambios en la config de webpack

--- website/webpack.config.js
+++ website/webpack.config.js
@@ -1,52 +1,102 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+// const HtmlWebPackPlugin = require("html-webpack-plugin");
 
-const htmlPlugin = new HtmlWebPackPlugin({
-  template: "./src/index.html",
-  filename: "./index.html"
-})
+// const htmlPlugin = new HtmlWebPackPlugin({
+//   template: "./src/index.html",
+//   filename: "./index.html"
+// })
+
+// module.exports = {
+//     target: 'web',
+//     entry: ['./src/index.js'],
+//     output: {
+//         publicPath: '/',
+//         path: resolve(__dirname, '..', 'build', 'client'),
+//         filename: '[name].js'
+//     },
+//     module: {
+//       rules: [
+//         {
+//           test: /\.js$/,
+//           exclude: /node_modules/,
+//           use: {
+//             loader: "babel-loader"
+//           }
+//         },
+//         {
+//             test: /\.css$/,
+//             use: [
+//                 {
+//                     loader: "style-loader"
+//                 },
+//                 {
+//                     loader: "css-loader",
+//                     options: {
+//                         modules: true,
+//                         importLoaders: 1,
+//                         localIdentName: "[name]_[local]_[hash:base64]",
+//                         sourceMap: true,
+//                         minimize: true
+//                     }
+//                 },
+//                 {
+//                 loader: 'postcss-loader', // Run post css actions
+//                 options: {
+//                     plugins: function () { // post css plugins, can be exported to postcss.config.js
+//                     return [
+//                         require('precss'),
+//                         require('autoprefixer')
+//                     ];
+//                     }
+//                 }
+//                 }, {
+//                 loader: 'sass-loader' // compiles Sass to CSS
+//                 }
+//             ]
+//         }
+//       ]
+//     },
+//     plugins: [htmlPlugin]
+//   }
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const outputDirectory = 'dist';
 
 module.exports = {
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
-          }
-        },
-        {
-            test: /\.css$/,
-            use: [
-                {
-                    loader: "style-loader"
-                },
-                {
-                    loader: "css-loader",
-                    options: {
-                        modules: true,
-                        importLoaders: 1,
-                        localIdentName: "[name]_[local]_[hash:base64]",
-                        sourceMap: true,
-                        minimize: true
-                    }
-                },
-                {
-                loader: 'postcss-loader', // Run post css actions
-                options: {
-                    plugins: function () { // post css plugins, can be exported to postcss.config.js
-                    return [
-                        require('precss'),
-                        require('autoprefixer')
-                    ];
-                    }
-                }
-                }, {
-                loader: 'sass-loader' // compiles Sass to CSS
-                }
-            ]
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, outputDirectory),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
         }
-      ]
-    },
-    plugins: [htmlPlugin]
-  }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+//   devServer: {
+//     port: 3000,
+//     open: true,
+//     proxy: {
+//       '/api': 'http://localhost:8080'
+//     }
+//   },
+  plugins: [
+    new CleanWebpackPlugin([outputDirectory]),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      favicon: './public/favicon.ico'
+    })
+  ]
+};
