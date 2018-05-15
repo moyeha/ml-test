6e02fc53f2064f0ebb4050244887ddf292bb78cb
Pedro Emiliano Moyano Cires <pedro.emiliano@gmail.com>
2018-05-15 00:09:27 -0300

    Modifique  la api de search y la de id

--- src/server/index.js
+++ src/server/index.js
@@ -1,15 +1,10 @@
 import express from 'express'
 import productRouter from './routes/product'
-// import searchRouter from './routes/search'
 const app = express()
-
-
-// app.use('/items', productRouter)
-// app.use('/search', searchRouter)
 
 app.use(express.static("dist"));
 app.use('/api/items', productRouter);
 app.listen(8080, function() {
-    console.log('eppepew');
+    console.log('The server is runing on port 8080');
     
 })
--- src/server/routes/product.js
+++ src/server/routes/product.js
@@ -1,28 +1,72 @@
 import express from 'express'
 import axios from 'axios'
+import R from 'ramda'
+const SEARCH_ENDPOINT = 'https://api.mercadolibre.com/sites/MLA/search'
+const ITEM_ENDPOINT = 'https://api.mercadolibre.com/items'
 
 const router = express.Router({
     mergeParams: true
 })
 
+const searchMap = item => ({
+    author: {
+        name: 'Pedro',
+        lastname: 'Moyano'
+    },
+    id: item.id,
+    title: item.title,
+    price: {
+        currency: item.currency_id,
+        amount: item.price,
+        decimal: 0
+    },
+    picture: item.thumbnail,
+    condition: item.condition,
+    free_shipping: item.shipping.free_shipping
+})
+
+const itemMap = item => ({
+    author: {
+        name: 'Pedro',
+        lastname: 'Moyano'
+    },
+    id: item.id,
+    title: item.title,
+    price: {
+        currency: item.currency_id,
+        amount: item.price,
+        decimal: 0
+    },
+    picture: item.thumbnail,
+    condition: item.condition,
+    free_shipping: item.free_shipping
+})
+
+
 router.all('/', (req, res) => {
-    
     axios
-        .get('https://api.mercadolibre.com/sites/MLA/search?q=' + req.query.q)
+        .get(`${SEARCH_ENDPOINT}?q=${req.query.q}`)
         .then(response => {
-            console.log('response', response.data)
-            res.send(response.data)
+            res.send(R.map(searchMap,response.data.results))
         })
         .catch(error => {
             console.log('error', error)
             res.send(error)
         });
-
-    
-    
 })
 router.get('/:id', (req, res) => {
-    res.send('por id')
+    // res.send(`${ITEM_ENDPOINT}/${req.params.id}`)
+    // return
+    axios
+        .get(`${ITEM_ENDPOINT}/${req.params.id}`)
+        .then(response => {
+            res.send(itemMap(response.data))
+            // res.send(R.map(searchMap,response.data.results))
+        })
+        .catch(error => {
+            console.log('error', error)
+            res.send(error)
+        });
 })
 
 router.get('/:id/:description', (req, res) => {
